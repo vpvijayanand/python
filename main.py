7d05daa7320b530b1f6f8bd50ebbe75042b81d9e
Vijayanand Premnath <vpvijayanand@gmail.com>
2023-11-14 09:40:19 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+RUN_INDENTED = False
+
+message = "running unindented"
+
+if RUN_INDENTED:
+    message = "running indented"
+
+print(message)
+
+
+def my_function():
+    greet = "Hello"
+    return greet
