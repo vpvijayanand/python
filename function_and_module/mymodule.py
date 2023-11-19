a3d9e44e86cee2fd5e4ae893eebd803753e18c04
Vijayanand Premnath <vpvijayanand@gmail.com>
2023-11-19 20:03:04 +0530

    Create mymodule.py

---
+++ function_and_module/mymodule.py
@@ -0,0 +1,12 @@
+"""
+Modules in Python are simply Python files with a .py extension. They can define functions, classes, and variables. A module can also include runnable code. You can create your own modules and use them in other parts of your Python program.
+"""
+# mymodule.py
+
+def add(a, b):
+    """Function to add two numbers"""
+    return a + b
+
+def multiply(a, b):
+    """Function to multiply two numbers"""
+    return a * b
