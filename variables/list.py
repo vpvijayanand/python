c5e5f49f0212b4cf57338b2ad00f461ff68c5cd2
Vijayanand Premnath <vpvijayanand@gmail.com>
2023-11-19 20:31:28 +0530

    Create list.py

---
+++ variables/list.py
@@ -0,0 +1,12 @@
+# Creating a list (a more flexible array-like structure)
+my_list = [1, 2, 3, 4]
+
+# Accessing list elements
+print(my_list[2])  # Output: 3
+
+# Lists are mutable
+my_list[2] = 30
+
+# Iterating through a list
+for element in my_list:
+    print(element)
