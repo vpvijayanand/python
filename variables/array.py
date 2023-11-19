92520a8176906e47958a2de20a1eaaf09e4bc5d1
Vijayanand Premnath <vpvijayanand@gmail.com>
2023-11-19 20:30:53 +0530

    Create array.py

---
+++ variables/array.py
@@ -0,0 +1,11 @@
+import array
+
+# Creating an array of integers
+my_array = array.array('i', [1, 2, 3, 4])
+
+# Accessing array elements
+print(my_array[2])  # Output: 3
+
+# Iterating through an array
+for element in my_array:
+    print(element)
