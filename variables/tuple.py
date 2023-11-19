529e9978c8e7461649369a43212ac5e25a448828
Vijayanand Premnath <vpvijayanand@gmail.com>
2023-11-19 20:29:36 +0530

    Create tuple.py

---
+++ variables/tuple.py
@@ -0,0 +1,12 @@
+# Creating a tuple
+my_tuple = (1, "Hello", 3.14)
+
+# Accessing tuple elements
+print(my_tuple[1])  # Output: Hello
+
+# Tuples are immutable, so you cannot change their values
+# my_tuple[1] = "World"  # This would raise an error
+
+# Iterating through a tuple
+for item in my_tuple:
+    print(item)
