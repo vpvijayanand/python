a97c67cc2fdb3407a22271b7711b024c73a8b01a
Vijayanand Premnath <vpvijayanand@gmail.com>
2023-11-19 19:29:42 +0530

    Create t2.py

---
+++ variables/t2.py
@@ -0,0 +1,4 @@
+2. Variable Names
+Rules for Naming: Variable names in Python can contain letters (a-z, A-Z), digits (0-9), and underscores (_). However, they cannot start with a digit.
+Case Sensitivity: Variable names are case-sensitive. For example, myVar, MyVar, and myvar are three different variables.
+Conventions: By convention, variable names should be lowercase with words separated by underscores (snake_case) for readability. For instance, my_variable.
