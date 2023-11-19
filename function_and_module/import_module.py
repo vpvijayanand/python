a71fb17bd61cc0304f5682b4dcb568addcbfe8e4
Vijayanand Premnath <vpvijayanand@gmail.com>
2023-11-19 20:03:56 +0530

    Create import_module.py

---
+++ function_and_module/import_module.py
@@ -0,0 +1,9 @@
+# import the module
+import mymodule
+
+# Use functions defined in the module
+result1 = mymodule.add(10, 5)
+result2 = mymodule.multiply(3, 4)
+
+print(f"Addition result: {result1}")
+print(f"Multiplication result: {result2}")
