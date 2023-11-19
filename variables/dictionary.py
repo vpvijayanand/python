9c179aa0a992addfb6292c962a418b25bf767831
Vijayanand Premnath <vpvijayanand@gmail.com>
2023-11-19 20:30:09 +0530

    Create dictionary.py

---
+++ variables/dictionary.py
@@ -0,0 +1,12 @@
+# Creating a dictionary
+my_dict = {"name": "Alice", "age": 25, "city": "New York"}
+
+# Accessing dictionary elements
+print(my_dict["name"])  # Output: Alice
+
+# Adding a new key-value pair
+my_dict["email"] = "alice@example.com"
+
+# Iterating through a dictionary
+for key, value in my_dict.items():
+    print(f"{key}: {value}")
