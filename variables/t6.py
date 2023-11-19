24f219ca6eb412d5c27bf0e84379470dbfc64e77
Vijayanand Premnath <vpvijayanand@gmail.com>
2023-11-19 19:34:50 +0530

    Create t6.py

---
+++ variables/t6.py
@@ -0,0 +1,8 @@
+#Constants
+#Python doesn't have built-in support for constants, but by convention, variables that are meant to be constants are written in all uppercase letters.
+PI = 3.14
+GRAVITY = 9.8
+#Deleting Variables
+#You can delete variables using the del statement.
+x = 5
+del x  # x is now deleted
