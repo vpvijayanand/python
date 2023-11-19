052bcb04e89bc2a3337ce56a0192a3656367ad64
Vijayanand Premnath <vpvijayanand@gmail.com>
2023-11-19 19:28:46 +0530

    Create t1.py

---
+++ variables/t1.py
@@ -0,0 +1,4 @@
+#Variable Declaration and Assignment
+#In Python, variables are created when you first assign a value to them. You don't need to declare a variable's type, as Python is dynamically typed. This means the Python interpreter infers the type of a variable at runtime.
+x = 5        # x is now an integer
+y = "Hello"  # y is now a string
