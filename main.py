1f02d9bb733fb906d24bda9336e216b1263a5478
Vijayanand Premnath <vpvijayanand@gmail.com>
2023-11-20 08:38:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,49 @@
-RUN_INDENTED = False
+import array
 
-message = "running unindented"
+# Define a basic class
+class Greeter:
+    def __init__(self, name):
+        self.name = name
 
-if RUN_INDENTED:
-    message = "running indented"
+    def greet(self):
+        return f"Hello, {self.name}!"
 
-print(message)
+# Function to demonstrate file operations
+def file_operations():
+    try:
+        # Writing to a file
+        with open('example.txt', 'w') as file:
+            file.write("This is a test file.\n")
+            file.write("Python is fun!")
 
+        # Reading from the file
+        with open('example.txt', 'r') as file:
+            content = file.read()
+            print("\nFile Content:\n", content)
 
-def my_function():
-    greet = "Hello"
-    return greet
+    except IOError as e:
+        print(f"File error: {e}")
+
+# Main function
+def main():
+    # Tuples and Dictionaries
+    my_tuple = (1, 2, 3)
+    my_dict = {'name': 'Alice', 'age': 30}
+
+    # Array
+    my_array = array.array('i', [1, 2, 3, 4])
+
+    # Using the Greeter class
+    greeter = Greeter("John")
+    print(greeter.greet())
+
+    # Working with data structures
+    print("\nTuple:", my_tuple)
+    print("Dictionary:", my_dict)
+    print("Array:", my_array.tolist())  # Converting array to list for easy viewing
+
+    # File operations
+    file_operations()
+
+if __name__ == "__main__":
+    main()
