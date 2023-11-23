66aaa620f7e043e55270a7af7ca0ead52a5cff0e
Vijayanand Premnath <vpvijayanand@gmail.com>
2023-11-23 09:10:22 +0530

    Update if_elif_else.py

--- control_flow_statement/if_elif_else.py
+++ control_flow_statement/if_elif_else.py
@@ -6,3 +6,21 @@ elif number < 20:
     print("Number is less than 20.")
 else:
     print("Number is 20.")
+
+
+#Using Dictionary to Mimic Switch-Case
+def switch_case(argument):
+    switcher = {
+        1: "Case 1: January",
+        2: "Case 2: February",
+        3: "Case 3: March",
+        # Add more cases as needed
+    }
+    # Get the function from switcher dictionary
+    return switcher.get(argument, "Invalid case")
+
+# Test the function
+print(switch_case(1))
+print(switch_case(3))
+print(switch_case(5))  # This will return 'Invalid case'
+
