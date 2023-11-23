32cec24aac0270f154f215fd6374cbf921b8e755
Vijayanand Premnath <vpvijayanand@gmail.com>
2023-11-23 09:13:02 +0530

    Update assert.py

--- control_flow_statement/assert.py
+++ control_flow_statement/assert.py
@@ -4,3 +4,16 @@ x = 5
 assert x == 5, "x should be 5"
 
 print("\nAll control flow statements demonstrated!")
+
+#Another example
+def divide(x, y):
+    assert y != 0, "Argument 'y' must not be zero."
+    return x / y
+
+# This will work
+result = divide(10, 2)
+print(result)
+
+# This will raise an AssertionError
+result = divide(10, 0)
+
